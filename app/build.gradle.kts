87eb8aa79e2721bf71a6ebe79ea4570d3af90c4d
Babak <145232367+BS1388@users.noreply.github.com>
2026-09-16 01:18:28 +0330

    Update versionCode from 1.1 to 1001

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "io.github.bs1388.ghostshare"
         minSdk = 27
         targetSdk = 35
-        versionCode = 1.1
+        versionCode = 1001
         versionName = "1.1"
     }
 
