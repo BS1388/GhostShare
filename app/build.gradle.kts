19a27f391ccea47de19eac550c2534b9b66f395c
Babak <145232367+BS1388@users.noreply.github.com>
2026-09-16 01:38:22 +0330

    Update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.bs1388.ghostshare"
         minSdk = 27
         targetSdk = 35
-        versionCode = 101
-        versionName = "1.0.1"
+        versionCode = 100
+        versionName = "1.0.0"
     }
 
     buildTypes {
