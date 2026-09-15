46a722df440600fdb2fe59554acba23815b576c8
Babak <145232367+BS1388@users.noreply.github.com>
2026-09-16 01:16:49 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.bs1388.ghostshare"
         minSdk = 27
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 1.1
+        versionName = "1.1"
     }
 
     buildTypes {
